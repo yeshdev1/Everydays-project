de8b4c28e762bc1877d36302585aa6de8a315e7f
alpha-personalSite-API <yeshdev@umich.edu>
2017-04-22 12:08:03 -0400

    reducer code changes

--- src/reducers/reducer_everydays_data.js
+++ src/reducers/reducer_everydays_data.js
@@ -1,11 +1,8 @@
 import ACTIONS  from '../utilities/constants';
 
 export default function everydays_data(state = [], action){
-  if(action.type === ACTIONS.DEFAULT_DATA){
+  if(action.type === ACTIONS.DEFAULT_DATA || action.type === ACTIONS.LAST_NAME_SORTED_DATA){
     return [...action.payload]; // Creating an entirely new object with the new state copied into it withe spread operator
-  }
-  else if(action.type === ACTIONS.LAST_NAME_SORTED_DATA) {
-    return [...action.payload];
   }
   return state;
 }
